a8363fdfff76b3f7d3953857b591eb6a6d30da42
Tristan Hörler <tristan.horler@gmail.com>
2020-06-05 07:27:52 +0200

    insert find and find_parent functions

--- binary-search-tree/class/node.rb
+++ binary-search-tree/class/node.rb
@@ -1,3 +1,13 @@
+class Node
+  attr_accessor :value, :left_child, :right_child
+
+  def initialize(value)
+    @value = value
+    @left_child = nil
+    @right_child = nil
+  end
+end
+
 module Comparable
   def compare(node_A, node_B)
     puts "A = #{node_A.value} et B = #{node_B.value}"
@@ -16,15 +26,5 @@ module Comparable
         compare(node_A.right_child, node_B)
       end
     end
-  end
-end
-
-class Node
-  attr_accessor :value, :left_child, :right_child
-
-  def initialize(value)
-    @value = value
-    @left_child = nil
-    @right_child = nil
   end
 end
--- binary-search-tree/class/tree.rb
+++ binary-search-tree/class/tree.rb
@@ -6,10 +6,11 @@ class Tree
 
   def initialize(array)
     @data = array.uniq
-    @root = build_tree(@data)
+    @root = build_tree
   end
 
-  def build_tree(array)
+  def build_tree
+    array = @data.dup
     root = Node.new(array.shift)
     array.each do |value|
       node = Node.new(value)
@@ -20,11 +21,78 @@ class Tree
 
   def insert(value)
     node = Node.new(value)
-    compare(root, value)
+    compare(root, node)
   end
 
-  def delete
-    #method which accepts a value to insert/delete (you’ll have to deal with
-    #several cases for delete such as when a node has children or not).
+  def delete(value, parent_pointer = @root)
+    node = find(value)
+    return nil if node.nil?
+
+    unless node == @root
+      parent = find_parent(value)
+      parent_pointer =
+        value < parent.value ? parent.left_child : parent.right_child
+    end
+
+    if node.left_child.nil?
+      return @parent_pointer = node.right_child
+    elsif node.right_child.nil?
+      return @parent_pointer = node.left_child
+    else
+      @parent_pointer = node.left_child
+      compare(node.left_child, node.right_child)
+    end
+    # unless node == @root
+    #   parent = find_parent(value)
+    #   parent_pointer =
+    #     value < parent.value ? parent.left_child : parent.right_child
+    #   if node.left_child.nil?
+    #     return parent_pointer = node.right_child
+    #   elsif node.right_child.nil?
+    #     return parent_pointer = node.left_child
+    #   else
+    #     parent_pointer = node.left_child
+    #     compare(node.left_child, node.right_child)
+    #   end
+    # else
+    #   if node.left_child.nil?
+    #     return @root = node.right_child
+    #   elsif node.right_child.nil?
+    #     return @root = node.left_child
+    #   else
+    #     @root = node.left_child
+    #     compare(node.left_child, node.right_child)
+    #   end
+    # end
+  end
+
+  def find(value, node = @root)
+    if value == node.value
+      return node
+    elsif value < node.value
+      return nil if node.left_child.nil?
+      find(value, node.left_child)
+    elsif value > node.value
+      return nil if node.right_child.nil?
+      find(value, node.right_child)
+    end
+  end
+
+  def find_parent(value)
+    value == @root.value ? nil : find_parent_rec(value)
+  end
+
+  def find_parent_rec(value, node = @root)
+    puts "#{value} = #{node.value} ?"
+    value_left_child = node.left_child.nil? ? nil : node.left_child.value
+    value_right_child = node.right_child.nil? ? nil : node.right_child.value
+
+    if value == value_left_child || value == value_right_child
+      return node
+    elsif value < node.value
+      value_left_child.nil? ? nil : find_parent_rec(value, node.left_child)
+    elsif value > node.value
+      value_right_child.nil? ? nil : find_parent_rec(value, node.right_child)
+    end
   end
 end
